13684ec6919af87bf1ea33a4b82d69fe940c8ad1
macus <andre@afsigma.com>
2016-05-29 22:30:25 -0400

    update queue time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel {
 		//$schedule->command('inspire')->hourly();
 
 	    $schedule->command('PublicEmailQwotes')
-				 ->hourly();			 
+				 ->dailyAt('8:00');			 
 	}
 
 }
